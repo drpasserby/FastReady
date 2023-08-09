2f8df9c15a44560bb8c4f50f23082f496da0aa9e
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2023-08-09 19:26:45 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MainTitle from './components/MainTitle.vue'
+import SwitchList from './components/SwitchList.vue'
+
+</script>
+
+<template>
+  <MainTitle />
+  <SwitchList />
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>

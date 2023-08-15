91b9dd66d65c7a3037131fa281f8862d45f38c09
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2023-08-15 10:27:23 +0800

    Update TakeList.vue
    修复截图背景颜色

--- src/components/TakeList.vue
+++ src/components/TakeList.vue
@@ -50,7 +50,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
         </button>
         <br>
-        <img :src="shotPicData" alt="保存截图" />
+        <img :src="shotPicData" alt="保存截图" class="showPicClass"/>
     </div>
   </template>
   
@@ -137,6 +137,7 @@
     }
     .takelistshow{
         /* width: 100%; */
+        background-color: #fff;
         border: 2px #cbcbcb solid;
         border-radius: 5px;
         padding: 0em 0.5em;
@@ -224,5 +225,6 @@
         font-size: 0.6em;
         color: #bbb;
     }
+    
   </style>
   

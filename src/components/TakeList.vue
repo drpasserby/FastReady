73479bd805df90b9c4f09aecc316838b50290223
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2023-09-11 00:17:42 +0800

    Update TakeList.vue
    更新防删措施和优化删除列表后显示逻辑

--- src/components/TakeList.vue
+++ src/components/TakeList.vue
@@ -70,6 +70,7 @@
         <button class="mybtn del" style="margin-bottom: 1em;" @click="closeScreenShots()">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
         </button>
+        <span><strong>&nbsp;长按保存</strong></span>
         <br>
         <img :src="shotPicData" alt="保存截图" class="showPicClass"/>
     </div>
@@ -141,10 +142,16 @@
         },
         //删除列表
         delList:function(e){
+            if(this.list.takeListList.length ==1){
+                alert('只剩下一个列表了,无法删除.')
+                return 0
+            }
             if(confirm('是否删除该列表?')){
                 if(confirm('删除后无法恢复,是否继续?') && this.list.takeListList.length > 1){
                     this.list.takeListList.splice(e,1)
                     this.updateListToLocal()
+                    //防止删除后列表为空
+                    this.whichList = 0               
                 }
             }
         },

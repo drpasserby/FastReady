e6147ff2c584292e9bcd94952588156933b853ca
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2023-10-24 23:55:13 +0800

    Update TakeList.vue
    添加 : 编辑选择列表功能

--- src/components/TakeList.vue
+++ src/components/TakeList.vue
@@ -7,6 +7,7 @@
             <button class="mybtn export" v-if="isShowFloatWin == 2" @click="addNewListGit">确认</button>
             <button class="mybtn export" v-if="isShowFloatWin == 3" @click="addNewItemGit">确认</button>
             <button class="mybtn export" v-if="isShowFloatWin == 4" @click="editItemGit">确认</button>
+            <button class="mybtn export" v-if="isShowFloatWin == 5" @click="editSelectListGit">确认</button>
             <button class="mybtn export" @click="isShowFloatWin = 0">取消</button>
         </div>
     </div>
@@ -60,7 +61,7 @@
                 </button>
             </span>
             <span>
-                <button class="mybtn edit" @click="test()">
+                <button class="mybtn edit" @click="editSelectList()">
                     <svg viewBox="0 0 24 24" width="17" height="17" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
                 </button>
             </span>
@@ -221,6 +222,10 @@
             else{
                 alert('物品名称为空,请输入.')
             }
+        },
+        //编辑选择列表
+        editSelectList:function(){
+            this.openFloatWin('编辑选择列表',"5")
         },
         //截图列表
         listScreenShots:function(){
